004c3bf58a09c05c0ebf509193a3496cf7b4708e
OffensivelyBad <OffensivelyBad@yahoo.com>
2018-04-26 23:00:24 -0400

    Adds barcode detection and sample images

--- BarcodeVision/BarcodeVision/MainViewController.swift
+++ BarcodeVision/BarcodeVision/MainViewController.swift
@@ -13,13 +13,18 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var inputImageView: UIImageView!
     
-    // Vision properties
-    private var barcodeRequest = VNDetectBarcodesRequest()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        detectTestImage()
+    }
+    
+    private func detectTestImage() {
+        guard let image = UIImage(named: "Rack") else { return }
+        inputImageView.image = image
+        inputImageView.contentMode = .scaleToFill
+        let barcodeRequest = getBarcodeRequest()
+        detectBarcode(in: image, and: barcodeRequest)
     }
 
 }
@@ -27,8 +32,8 @@ class MainViewController: UIViewController {
 // MARK: - Vision
 extension MainViewController {
     
-    private func setupBarcodeRequest() {
-        barcodeRequest = VNDetectBarcodesRequest(completionHandler: { (request, error) in
+    private func getBarcodeRequest() -> VNDetectBarcodesRequest {
+        return VNDetectBarcodesRequest(completionHandler: { [unowned self] (request, error) in
             guard let results = request.results else {
                 // TODO: handle no barcodes found
                 return
@@ -38,10 +43,39 @@ extension MainViewController {
                 print(barcode.payloadStringValue ?? "", terminator: "\n\n")
                 
                 if let rect = result as? VNRectangleObservation {
-                    // TODO: draw a box around the barcode
+                    self.draw(box: rect)
                 }
             }
         })
+    }
+    
+    private func detectBarcode(in image: UIImage, and request: VNDetectBarcodesRequest) {
+        guard let imageCG = image.cgImage else { return }
+        let imageHandler = VNImageRequestHandler(cgImage: imageCG, options: [.properties: ""])
+        do {
+            try imageHandler.perform([request])
+        } catch {
+            // TODO: handle errors
+            print("error handling image request")
+        }
+    }
+    
+}
+
+// MARK: - Rendering
+extension MainViewController {
+    
+    func draw(box: VNRectangleObservation) {
+        let x = box.topLeft.x * inputImageView.frame.size.width
+        let y = (1 - box.topLeft.y) * inputImageView.frame.size.height
+        let width = (box.topRight.x - box.bottomLeft.x) * inputImageView.frame.size.width
+        let height = (box.topLeft.y - box.bottomLeft.y) * inputImageView.frame.size.height
+        
+        let layer = CALayer()
+        layer.borderWidth = 2
+        layer.frame = CGRect(x: x - layer.borderWidth, y: y - layer.borderWidth, width: width + layer.borderWidth * 2, height: height + layer.borderWidth * 2)
+        layer.borderColor = UIColor.red.cgColor
+        inputImageView.layer.addSublayer(layer)
     }
     
 }
